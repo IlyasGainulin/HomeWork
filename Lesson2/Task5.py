dd7c37cc2ee2fa1dc07fae3ce308e0c55b7a4a65
IlyasGainulin <112005564+IlyasGainulin@users.noreply.github.com>
2022-11-29 19:33:44 +0400

    Задание 5

--- Lesson2/Task5.py
+++ Lesson2/Task5.py
@@ -1,26 +1,6 @@
 # Реализуйте алгоритм перемешивания списка.
-def ignore(vodMas, count):
-    n = 0
-    while n in vodMas:
-        n = random.randint(0, count*2-1)
-
-    vodMas.append(n)
-    return n
-
-
-def shuffleMas(mas):
-    ignoreMas = []
-    count = int(len(mas)/2)
-    # print(count)
-    for i in range(count):
-        first = ignore(ignoreMas, count)
-        second = ignore(ignoreMas, count)
-        swap = mas[first]
-        mas[first] = mas[second]
-        mas[second] = swap
-    print(f"Перемешанный массив: {mas}")
-
-
-a = ["1", "2", "3", "4", "5", "6"]
-
-shuffleMas(a)
+list = ['Привет', 47.8, 962, 'Пока ', '+-', '..']
+print('Список: ', list) 
+import random
+random.shuffle(list)
+print('Перемешанный список: ', list) 
