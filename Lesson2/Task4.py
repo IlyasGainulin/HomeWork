1fd340d5fd0f642bb2457529194336be8918c451
IlyasGainulin <112005564+IlyasGainulin@users.noreply.github.com>
2022-11-29 19:38:13 +0400

    Задание 4

--- Lesson2/Task4.py
+++ Lesson2/Task4.py
@@ -1,14 +1,13 @@
 # Задайте список из N элементов, заполненных числами из промежутка [-N, N]. Найдите произведение элементов на указанных позициях. Позиции хранятся в файле file.txt в одной строке одно число.
 # (для продвинутых - с файлом, вариант минимум - ввести позиции в консоли) -2 -1 0 1 2 Позиции: 0,1 -> 2
 # with open("file.txt", "r") as f:
-#     firstIndex = int(f.readline())
-#     secondIndex = int(f.readline())
-
-
-def collection(n):
-    result24 = []
-    for i in range(-n, n+1):
-        result24.append(i)
-    print(result24)
-    mult = result24[firstIndex]*result24[secondIndex]
-    print(mult)
+n = int(input('Введите число --> '))
+def composition_element(n):
+    for i in range(-n, n):
+        x = randint(-n, n)
+        y = randint(-n, n)
+        result = x*y
+    print(f'[{-n} , {n}]')
+    print(f'одно число из промежутка = {x}, второе число из промежутка = {y}')
+    return result
+print(f'произведение равно {composition_element(n)}')
